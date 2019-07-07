954a4c5a15da79282a73500489820a4c5f202ac9
Yuri Matheus <yurimatheusb@hotmail.com>
2019-07-07 16:25:10 -0300

    creates front project

---
+++ consulta-delfos/src/main/java/br/com/fiap/mpsp/consultadelfos/ConsultaDelfosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mpsp.consultadelfos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaDelfosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaDelfosApplication.class, args);
+	}
+
+}
