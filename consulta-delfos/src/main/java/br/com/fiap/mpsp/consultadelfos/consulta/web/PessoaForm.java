a1dac00d5d4a152c1f0e5e80560952b607535238
Gabriel <gabriheron@hotmail.com>
2019-09-27 00:34:48 -0300

    [Novos Campos] Adicionando novos campos no modelo

--- consulta-delfos/src/main/java/br/com/fiap/mpsp/consultadelfos/consulta/web/PessoaForm.java
+++ consulta-delfos/src/main/java/br/com/fiap/mpsp/consultadelfos/consulta/web/PessoaForm.java
@@ -1,5 +1,6 @@
 package br.com.fiap.mpsp.consultadelfos.consulta.web;
 
+import org.hibernate.validator.constraints.br.CNPJ;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.NotBlank;
@@ -16,6 +17,12 @@ class PessoaForm {
     private LocalDate dataDeNascimento;
     @CPF
     private String cpf;
+    @CNPJ
+    private String cnpj;
+    @NotBlank
+    private String numeroProcesso;
+    @NotBlank
+    private String pis;
 
     /**
     * apenas para uso de frameworks
@@ -23,26 +30,69 @@ class PessoaForm {
     @Deprecated
     PessoaForm() {}
 
-    PessoaForm(@NotBlank String nome, @NotBlank String nomeDaMae, @NotNull LocalDate dataDeNascimento, @CPF String cpf) {
+    public PessoaForm(@NotBlank String nome, @NotBlank String nomeDaMae, @NotNull LocalDate dataDeNascimento, @CPF String cpf, @CNPJ String cnpj, @NotBlank String numeroProcesso, @NotBlank String pis) {
         this.nome = nome;
         this.nomeDaMae = nomeDaMae;
         this.dataDeNascimento = dataDeNascimento;
         this.cpf = cpf;
+        this.cnpj = cnpj;
+        this.numeroProcesso = numeroProcesso;
+        this.pis = pis;
     }
 
     public String getNome() {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getNomeDaMae() {
         return nomeDaMae;
+    }
+
+    public void setNomeDaMae(String nomeDaMae) {
+        this.nomeDaMae = nomeDaMae;
     }
 
     public LocalDate getDataDeNascimento() {
         return dataDeNascimento;
     }
 
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
     public String getCpf() {
         return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNumeroProcesso() {
+        return numeroProcesso;
+    }
+
+    public void setNumeroProcesso(String numeroProcesso) {
+        this.numeroProcesso = numeroProcesso;
+    }
+
+    public String getPis() {
+        return pis;
+    }
+
+    public void setPis(String pis) {
+        this.pis = pis;
     }
 }
