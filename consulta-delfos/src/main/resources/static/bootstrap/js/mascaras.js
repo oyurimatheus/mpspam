f52f9970e2bf43ee4ab858b9fe94483907f62687
Gabriel <gabriheron@hotmail.com>
2019-09-27 00:35:32 -0300

    [Novos Campos] Adicionando novos arquivos de bootstrap e vanilla masker

---
+++ consulta-delfos/src/main/resources/static/bootstrap/js/mascaras.js
@@ -0,0 +1,4 @@
+VMasker(document.querySelector("#cpf")).maskPattern("999.999.999-99")
+VMasker(document.querySelector("#cnpj")).maskPattern("99.999.999/9999-99")
+VMasker(document.querySelector("#numeroProcesso")).maskPattern("9999999-99.9999.999.9999")
+VMasker(document.querySelector("#pis")).maskPattern("999.99999.99-9")
