4b99c70b372f1d0d37c6ebce5e7032a5aaf12a90
Gabriel <gabriheron@hotmail.com>
2019-10-01 20:33:21 -0300

    [Crawler] Adicionando censec no relatorio

--- consulta-delfos/src/main/java/br/com/fiap/mpsp/consultadelfos/relatorio/RelatorioController.java
+++ consulta-delfos/src/main/java/br/com/fiap/mpsp/consultadelfos/relatorio/RelatorioController.java
@@ -8,6 +8,8 @@ import br.com.fiap.mpsp.consultadelfos.resposta.listener.cadesp.CadespRepository
 import br.com.fiap.mpsp.consultadelfos.resposta.listener.cadesp.CadespResposta;
 import br.com.fiap.mpsp.consultadelfos.resposta.listener.caged.CagedRepository;
 import br.com.fiap.mpsp.consultadelfos.resposta.listener.caged.CagedResposta;
+import br.com.fiap.mpsp.consultadelfos.resposta.listener.censec.CensecRepository;
+import br.com.fiap.mpsp.consultadelfos.resposta.listener.censec.CensecResposta;
 import br.com.fiap.mpsp.consultadelfos.resposta.listener.siel.SielRepository;
 import br.com.fiap.mpsp.consultadelfos.resposta.listener.siel.SielResposta;
 import org.springframework.stereotype.Controller;
@@ -15,10 +17,8 @@ import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Objects;
 import java.util.Optional;
 
 import static java.util.Objects.isNull;
@@ -32,17 +32,20 @@ public class RelatorioController {
     private final CadespRepository cadespRepository;
     private final CagedRepository cagedRepository;
     private final SielRepository sielRepository;
+    private final CensecRepository censecRepository;
 
     public RelatorioController(PessoaRepository pessoaRepository,
                                ArpenspRepository arpenspRepository,
                                CadespRepository cadespRepository,
                                CagedRepository cagedRepository,
-                               SielRepository sielRepository) {
+                               SielRepository sielRepository,
+                               CensecRepository censecRepository) {
         this.pessoaRepository = pessoaRepository;
         this.arpenspRepository = arpenspRepository;
         this.cadespRepository = cadespRepository;
         this.cagedRepository = cagedRepository;
         this.sielRepository = sielRepository;
+        this.censecRepository = censecRepository;
     }
 
     @GetMapping("/pesquisa")
@@ -51,7 +54,7 @@ public class RelatorioController {
     }
 
     @GetMapping
-    public String relatorio(@RequestParam("cpf") String cpf, Model model, RedirectAttributes redirectAttributes) {
+    public String relatorio(String cpf, Model model) {
         if(isNull(cpf)) {
             model.addAttribute("pessoas", pessoaRepository.findAll());
             return "relatorios/lista";
@@ -62,12 +65,14 @@ public class RelatorioController {
         Optional<CadespResposta> cadesp = cadespRepository.findById(pessoa.getId());
         Optional<CagedResposta> caged = cagedRepository.findById(pessoa.getId());
         Optional<SielResposta> siel = sielRepository.findById(pessoa.getId());
+        Optional<CensecResposta> censec = censecRepository.findById(pessoa.getId());
 
         model.addAttribute("pessoa", pessoa);
         model.addAttribute("arpensp", arpensp.orElse(new ArpenspResposta()));
         model.addAttribute("cadesp", cadesp.orElse(new CadespResposta()));
         model.addAttribute("caged", caged.orElse(new CagedResposta()));
         model.addAttribute("siel", siel.orElse(new SielResposta()));
+        model.addAttribute("censec", censec.orElse(new CensecResposta()));
 
         return "relatorios/relatorio";
     }
